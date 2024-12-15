f1d03a1b0b74a59f7e9a93b5a9034eaa13a6c42d
FlyingShiba <flyingshibaconecheddar@gmail.com>
2024-12-15 04:05:48 -0300

    edit22

--- js/script.js
+++ js/script.js
@@ -1,24 +1,40 @@
-// Configura aquí la imagen que quieres mostrar
-const imageToShow = 'none'; // Opciones: 'jpg', 'png', o 'none'
+// Configura aquí el medio que quieres mostrar
+const mediaToShow = 'png'; // Opciones: 'jpg', 'png', 'video', o 'none'
 
 document.addEventListener('DOMContentLoaded', function() {
+    const centralContent = document.querySelector('.central-content');
     const centralImage = document.getElementById('centralImage');
+    const centralVideo = document.getElementById('centralVideo');
     
-    switch(imageToShow) {
-        case 'jpg':
-            centralImage.src = './assets/800600.jpg';
-            centralImage.style.display = 'block';
-            break;
-        case 'png':
-            centralImage.src = './assets/nueva-imagen.png';
-            centralImage.style.display = 'block';
-            break;
-        case 'none':
-            centralImage.style.display = 'none';
-            break;
-        default:
-            console.error('Opción de imagen no válida');
-            centralImage.style.display = 'none';
+    function updateMedia(type) {
+        switch(type) {
+            case 'jpg':
+                centralContent.style.backgroundImage = "url('./assets/800600.jpg')";
+                centralImage.style.display = 'none';
+                centralVideo.style.display = 'none';
+                break;
+            case 'png':
+                centralContent.style.backgroundImage = "none";
+                centralImage.src = './assets/nueva-imagen.png';
+                centralImage.style.display = 'block';
+                centralVideo.style.display = 'none';
+                break;
+            case 'video':
+                centralContent.style.backgroundImage = "none";
+                centralImage.style.display = 'none';
+                centralVideo.style.display = 'block';
+                centralVideo.play();
+                break;
+            case 'none':
+                centralContent.style.backgroundImage = "none";
+                centralImage.style.display = 'none';
+                centralVideo.style.display = 'none';
+                break;
+            default:
+                console.error('Opción de medio no válida');
+        }
     }
+
+    updateMedia(mediaToShow);
 });
 
