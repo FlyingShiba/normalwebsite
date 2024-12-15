76a48bdbb8cc207dae00913b79b4b9c9a5704a3a
FlyingShiba <flyingshibaconecheddar@gmail.com>
2024-12-15 03:55:02 -0300

    js edit

--- js/script.js
+++ js/script.js
@@ -1,13 +1,24 @@
 // Configura aquí la imagen que quieres mostrar
-const showPngImage = true; // Cambia a true para mostrar la imagen PNG, false para la JPG
+const imageToShow = 'none'; // Opciones: 'jpg', 'png', o 'none'
 
 document.addEventListener('DOMContentLoaded', function() {
     const centralImage = document.getElementById('centralImage');
     
-    if (showPngImage) {
-        centralImage.src = './assets/nueva-imagen.png';
-    } else {
-        centralImage.src = './assets/800600.jpg';
+    switch(imageToShow) {
+        case 'jpg':
+            centralImage.src = './assets/800600.jpg';
+            centralImage.style.display = 'block';
+            break;
+        case 'png':
+            centralImage.src = './assets/nueva-imagen.png';
+            centralImage.style.display = 'block';
+            break;
+        case 'none':
+            centralImage.style.display = 'none';
+            break;
+        default:
+            console.error('Opción de imagen no válida');
+            centralImage.style.display = 'none';
     }
 });
 
